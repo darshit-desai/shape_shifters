69236a6c0eb0eb8d479e06c06e3a722f1d63b227
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-12-18 23:35:51 -0500

    Task 2.2 1.) resolved the namespace issue while launching

--- launch/gazebo_launch.py
+++ launch/gazebo_launch.py
@@ -38,6 +38,7 @@ def generate_launch_description():
             output='screen',
             arguments=[
                 '-entity', f'turtlebot{i}',
+                "-robot_namespace", f"tb{i}",
                 '-file', f'/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf',
                 '-x', str(i * spacing_x),  # Adjust the x pose
                 '-y', '0.0',  # Adjust the y pose
