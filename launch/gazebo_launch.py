8fe0911c3d256d247080412f9b6611a62b0877eb
shivamsehgal77 <112571645+shivamsehgal77@users.noreply.github.com>
2023-12-19 00:26:17 -0500

    2.2 1.) Added timer actions to launch the turtlebot after specified time

--- launch/gazebo_launch.py
+++ launch/gazebo_launch.py
@@ -1,11 +1,8 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch_ros.actions import Node
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import numpy as np
 
@@ -31,26 +28,43 @@ def generate_launch_description():
         ])
     )
 
-    x_rand = np.random.uniform(-10, 10, num_turtlebots)
-    y_rand = np.random.uniform(-10, 10, num_turtlebots)
+    # Generate random positions for TurtleBots within a reasonable range
+    x_rand = np.random.uniform(-5, 5, num_turtlebots)
+    y_rand = np.random.uniform(-5, 5, num_turtlebots)
+
     # Create nodes to spawn multiple TurtleBots
-    spawn_turtlebots = [
-        Node(
+    spawn_turtlebots = []
+    for i in range(num_turtlebots):
+        node = Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
             output='screen',
+            namespace=f'turtlebot{i}',
             arguments=[
                 '-entity', f'turtlebot{i}',
-                "-robot_namespace", f"tb{i}",
-                '-file', f'/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_burger/model.sdf',
+                '-file', f'/opt/ros/humble/share/turtlebot3_gazebo/models/turtlebot3_{turtlebot_model}/model.sdf',
                 '-x', str(x_rand[i]),  # Adjust the x pose
                 '-y', str(y_rand[i]),  # Adjust the y pose
                 '-z', '0.01'
             ],
-        ) for i in range(num_turtlebots)
-    ]
+        )
+        spawn_turtlebots.append(node)
 
-    return LaunchDescription([
+        # Add TimerAction for introducing a delay before each spawn
+        timer_action = TimerAction(
+            period=10.0 + float(i * 2),  # Adjust the delay period
+            actions=[node],
+        )
+        ld.add_action(timer_action)
+
+    ld = LaunchDescription([
         empty_world_launch,
         *spawn_turtlebots
     ])
+
+    # Print information for debugging
+    print("Number of TurtleBots:", num_turtlebots)
+    print("Spawn positions (x):", x_rand)
+    print("Spawn positions (y):", y_rand)
+
+    return ld
